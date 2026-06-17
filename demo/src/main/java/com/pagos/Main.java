e92b443312a2e349e916528ddb01bd85972cf448
Otho-Code <oacantos@uce.edu.ec>
2026-06-16 23:07:59 -0500

    Esta es la buena

--- demo/src/main/java/com/pagos/Main.java
+++ demo/src/main/java/com/pagos/Main.java
@@ -4,16 +4,20 @@ public class Main {
     public static void main(String[] args) {
         
         Transferencia transferencia1 = new Transferencia("1234567890", "Pichincha", "1728203926");
-        transferencia1.pagar(100);
-
         TarjetaCredito tarjetaCredito1 = new TarjetaCredito("156164961641", "Maicky", "12/33", "258");
-        tarjetaCredito1.pagar(2000);
+
+
 
         CarritoCompras carrito = new CarritoCompras();
         carrito.agregarProducto(new Producto("Laptop", 1500));
         carrito.agregarProducto(new Producto("Smartphone", 800));
         carrito.agregarProducto(new Producto("Audífonos", 200));
         carrito.mostrarProductos();
-        carrito.pagar(tarjetaCredito1); 
+        System.out.println();
+        System.out.println("Pago con transferencia");
+        carrito.pagar(transferencia1); 
+        System.out.println();
+        System.out.println("Pago con tarjeta de credito");
+        carrito.pagar(tarjetaCredito1);
     }
 }
